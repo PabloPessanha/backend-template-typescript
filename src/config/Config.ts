a5487444dffe30c26945605f1505ef69a9605eb5
Pablo Pessanha <pablopessanh@gmail.com>
2023-03-06 15:14:52 -0300

    feat: removes database configuration; adds prisma.schema for configuration

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -6,14 +6,6 @@ export class Config {
   public readonly port = Number(process.env.PORT) || 4444;
   public readonly host = process.env.API_HOST || `http://localhost:${this.port}`;
 
-  public readonly database = {
-    host: process.env.DATABASE_HOST || '',
-    port: Number(process.env.DATABASE_PORT) || 5432,
-    user: process.env.POSTGRES_USER || 'test',
-    password: process.env.POSTGRES_PASSWORD || 'test',
-    name: process.env.POSTGRES_DATABASE || 'test',
-  };
-
   public static isDev() {
     return process.env.NODE_ENV === 'development';
   }
